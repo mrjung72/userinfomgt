afbd5ed02253e1cec2131561dc532d15e2d256d9
sahara <mrjung72@gmail.com>
2025-03-18 01:00:19 +0900

    사용자정보 가져오는 프로그램 추가

---
+++ userinfo.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import pymysql
+
+app = Flask(__name__)
+
+db_config = {
+    "host": "localhost",
+    "user": "sahara",
+    "password": "7803",
+    "database": "userinfodb",
+    "port": 3306,
+    "cursorclass": pymysql.cursors.DictCursor
+}
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    try:
+        connection = pymysql.connect(**db_config)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM users")
+            users = cursor.fetchall()
+            return jsonify(users)  # JSON 반환
+    finally:
+        connection.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
